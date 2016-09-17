44a557cb5594c8686299142449982dafb4313930
陈奇 <892854990@qq.com>
2016-09-17 11:28:35 +0800

    2016/9/16/:11:28

---
+++ src/com/chen/client/myjdbc/Dome.java
@@ -0,0 +1,94 @@
+package com.chen.client.myjdbc;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by 陈奇
+ * on 2016/6/2 0002.
+ */
+public class Dome {
+    public static void main(String[] args) throws Exception {
+
+        read("");
+    }
+
+    public static String read(String name) throws Exception {
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            conn = JdbcTool2.getConnection();
+//创建SQL语句
+
+            String sql = "select user_id, name, pass_word from user";
+            ps = conn.prepareStatement(sql);
+//            ps.setString(1, name);
+
+
+//            String sql = "delete from user where id>2";
+//            执行语句
+            rs = ps.executeQuery();
+            while (rs.next()) {
+
+                System.out.println(rs.getObject("userId") + "\t  "
+                        + rs.getObject("username") + "\t" + rs.getObject("passwd"));
+
+                Object ob = rs.getObject("passwd");
+                System.out.println(ob.toString());
+                return ob.toString();
+            }
+            return null;
+        } finally {
+            JdbcTool2.free(rs, ps, conn);
+        }
+    }
+
+@Test
+    public static List<User> readmytable() throws Exception {
+
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+
+            conn = JdbcTool2.getConnection();
+//创建SQL语句
+
+            String sql = "select * from users ";
+            ps = conn.prepareStatement(sql);
+//            ps.setString(1, name);
+
+
+//            String sql = "delete from user where id>2";
+//            执行语句
+            rs = ps.executeQuery();
+
+            List<User> res = new ArrayList<User>();
+            while (rs.next()) {
+                User user = new User();
+                user.setUser_id(rs.getInt("userId"));
+                user.setName(rs.getString(2));
+                user.setEmail(rs.getString(4));
+                user.setGrade(rs.getInt(5));
+
+                res.add(user);
+            }
+
+
+            return res;
+        } finally {
+            JdbcTool2.free(rs, ps, conn);
+        }
+    }
+}
---
+++ web/common/common.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2016/9/17 0017
+  Time: 0:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page language="java" import="java.util.*" pageEncoding="utf-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+
+
+    <c:set var="proPath" value="${pageContext.request.contextPath}"/>
+    <link rel="stylesheet" type="text/css"
+          href="${proPath}/lib/jquery-easyui-1.4.1/themes/default/easyui.css">
+    <link rel="stylesheet" type="text/css" href="${proPath}/lib/jquery-easyui-1.4.1/themes/icon.css">
+    <script type="text/javascript" src="${proPath}/lib/jquery-easyui-1.4.1/jquery.min.js"></script>
+    <script type="text/javascript" src="${proPath}/lib/jquery-easyui-1.4.1/jquery.easyui.min.js"></script>
+<script type="text/javascript" src="${proPath}/lib/jquery-easyui-1.4.1/locale/easyui-lang-zh_CN.js"></script>
